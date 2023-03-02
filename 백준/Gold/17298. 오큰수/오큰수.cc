b67ae760c6155a51c4dd2af23ef76812019eb6f8
se-ize <70623290+se-ize@users.noreply.github.com>
2023-03-02 20:23:22 +0900

    [Gold IV] Title: 오큰수, Time: 248 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N;
+    cin >> N;
+    vector<int> A(N, 0);
+    vector<int> ans(N, 0);
+    
+    for(int i=0; i<N; i++){
+        cin >> A[i];
+    }
+    
+    stack<int> stack;
+    stack.push(0);
+    
+    for(int i=1; i<N; i++){
+        while(!stack.empty() && A[stack.top()] < A[i]) {
+            ans[stack.top()] = A[i];
+            stack.pop();
+        }
+        stack.push(i);
+    }
+    while(!stack.empty()){
+        ans[stack.top()] = -1;
+        stack.pop();
+    }
+    for(int i=0; i<N; i++){
+        cout << ans[i] << " ";
+    }
+}
